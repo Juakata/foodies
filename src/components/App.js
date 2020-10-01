518eb803410e6bc74f980d22eae64e1a61f57840
Juakata <juakata07@gmail.com>
2020-09-30 22:19:33 -0600

    add components to Home

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './home';
+import Header from './shared/header';
 import ScrollTop from './ScrollToTop';
 
 class App extends React.Component {
   render() {
     return (
       <BrowserRouter>
+        <Header />
         <Switch>
           <ScrollTop>
             <Route exact path="/" component={Home} />
--- src/components/home/index.js
+++ src/components/home/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import Welcome from '../shared/welcome';
+import About from './about';
+import Locations from './locations';
+import Hamburger from '../../assets/main/hamburger.png';
 import './style.scss';
 
 const Home = () => (
-  <div>
-    Hola
-  </div>
+  <>
+    <Welcome
+      text1="Un nuevo"
+      text2="sabor esta en"
+      text3="la ciudad"
+      img={Hamburger}
+      backgroundClass="hamburger-1"
+      findUs
+    />
+    <About />
+    <Locations />
+  </>
 );
 
 export default Home;
